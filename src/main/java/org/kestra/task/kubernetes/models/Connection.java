99588ef2006230653eede76c897d14be8e0a03fb
Eric Régnier <eric.regnier@ext.leroymerlin.fr>
2020-09-24 10:14:23 +0200

    feat(tasks): Add missing oauthToken authentication
    also add some documentation & render properties

--- src/main/java/org/kestra/task/kubernetes/models/Connection.java
+++ src/main/java/org/kestra/task/kubernetes/models/Connection.java
@@ -4,32 +4,124 @@ import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import lombok.Builder;
 import lombok.Getter;
+import org.kestra.core.exceptions.IllegalVariableEvaluationException;
+import org.kestra.core.models.annotations.InputProperty;
+import org.kestra.core.runners.RunContext;
 
 @Builder
 @Getter
 public class Connection {
-    private Boolean trustCerts;
-    private Boolean disableHostnameVerification;
-    @Builder.Default
-    private String masterUrl = "https://kubernetes.default.svc";
-    @Builder.Default
-    private String apiVersion = "v1";
-    private String namespace;
-    private String caCertFile;
-    private String caCertData;
-    private String clientCertFile;
-    private String clientCertData;
-    private String clientKeyFile;
-    private String clientKeyData;
-    @Builder.Default
-    private String clientKeyAlgo = "RSA";
-    private String clientKeyPassphrase;
-    private String trustStoreFile;
-    private String trustStorePassphrase;
-    private String keyStoreFile;
-    private String keyStorePassphrase;
+    @InputProperty(
+        description = "Trust all certificates"
+    )
+    private final Boolean trustCerts;
 
-    public Config toConfig() {
+    @InputProperty(
+        description = "Disable hostname verification"
+    )
+    private final Boolean disableHostnameVerification;
+
+    @InputProperty(
+        description = "The url to kubernetes API",
+        dynamic = true
+    )
+    @Builder.Default
+    private final String masterUrl = "https://kubernetes.default.svc";
+
+    @InputProperty(
+        description = "The api version of API to use",
+        dynamic = true
+    )
+    @Builder.Default
+    private final String apiVersion = "v1";
+
+    @InputProperty(
+        description = "The namespace used",
+        dynamic = true
+    )
+    private final String namespace;
+
+    @InputProperty(
+        description = "CA certificate as file path",
+        dynamic = true
+    )
+    private final String caCertFile;
+
+    @InputProperty(
+        description = "CA certificate as data (",
+        dynamic = true
+    )
+    private final String caCertData;
+
+    @InputProperty(
+        description = "Client certificate as file path",
+        dynamic = true
+    )
+    private final String clientCertFile;
+
+    @InputProperty(
+        description = "Client certificate as data",
+        dynamic = true
+    )
+    private final String clientCertData;
+
+    @InputProperty(
+        description = "Client Key as file path",
+        dynamic = true
+    )
+    private final String clientKeyFile;
+
+    @InputProperty(
+        description = "Client Key as data",
+        dynamic = true
+    )
+    private final String clientKeyData;
+
+    @InputProperty(
+        description = "Client key encryption algorithm",
+        body = "default is RSA",
+        dynamic = true
+    )
+    @Builder.Default
+    private final String clientKeyAlgo = "RSA";
+
+    @InputProperty(
+        description = "Client key passphrase",
+        dynamic = true
+    )
+    private final String clientKeyPassphrase;
+
+    @InputProperty(
+        description = "Truststore file",
+        dynamic = true
+    )
+    private final String trustStoreFile;
+
+    @InputProperty(
+        description = "Truststore passphrase",
+        dynamic = true
+    )
+    private final String trustStorePassphrase;
+
+    @InputProperty(
+        description = "Key store file",
+        dynamic = true
+    )
+    private final String keyStoreFile;
+
+    @InputProperty(
+        description = "Key store passphrase",
+        dynamic = true
+    )
+    private final String keyStorePassphrase;
+
+    @InputProperty(
+        description = "Oauth token",
+        dynamic = true
+    )
+    private final String oauthToken;
+
+    public Config toConfig(RunContext runContext) throws IllegalVariableEvaluationException {
         ConfigBuilder builder = new ConfigBuilder();
 
         if (trustCerts != null) {
@@ -41,63 +133,67 @@ public class Connection {
         }
 
         if (masterUrl != null) {
-            builder.withMasterUrl(masterUrl);
+            builder.withMasterUrl(runContext.render(masterUrl));
         }
 
         if (apiVersion != null) {
-            builder.withApiVersion(apiVersion);
+            builder.withApiVersion(runContext.render(apiVersion));
         }
 
         if (namespace != null) {
-            builder.withNamespace(namespace);
+            builder.withNamespace(runContext.render(namespace));
         }
 
         if (caCertFile != null) {
-            builder.withCaCertFile(caCertFile);
+            builder.withCaCertFile(runContext.render(caCertFile));
         }
 
         if (caCertData != null) {
-            builder.withCaCertData(caCertData);
+            builder.withCaCertData(runContext.render(caCertData));
         }
 
         if (clientCertFile != null) {
-            builder.withClientCertFile(clientCertFile);
+            builder.withClientCertFile(runContext.render(clientCertFile));
+        }
+
+        if (oauthToken != null) {
+            builder.withOauthToken(runContext.render(oauthToken));
         }
 
         if (clientCertData != null) {
-            builder.withClientCertData(clientCertData);
+            builder.withClientCertData(runContext.render(clientCertData));
         }
 
         if (clientKeyFile != null) {
-            builder.withClientKeyFile(clientKeyFile);
+            builder.withClientKeyFile(runContext.render(clientKeyFile));
         }
 
         if (clientKeyData != null) {
-            builder.withClientKeyData(clientKeyData);
+            builder.withClientKeyData(runContext.render(clientKeyData));
         }
 
         if (clientKeyAlgo != null) {
-            builder.withClientKeyAlgo(clientKeyAlgo);
+            builder.withClientKeyAlgo(runContext.render(clientKeyAlgo));
         }
 
         if (clientKeyPassphrase != null) {
-            builder.withClientKeyPassphrase(clientKeyPassphrase);
+            builder.withClientKeyPassphrase(runContext.render(clientKeyPassphrase));
         }
 
         if (trustStoreFile != null) {
-            builder.withTrustStoreFile(trustStoreFile);
+            builder.withTrustStoreFile(runContext.render(trustStoreFile));
         }
 
         if (trustStorePassphrase != null) {
-            builder.withTrustStorePassphrase(trustStorePassphrase);
+            builder.withTrustStorePassphrase(runContext.render(trustStorePassphrase));
         }
 
         if (keyStoreFile != null) {
-            builder.withKeyStoreFile(keyStoreFile);
+            builder.withKeyStoreFile(runContext.render(keyStoreFile));
         }
 
         if (keyStorePassphrase != null) {
-            builder.withKeyStorePassphrase(keyStorePassphrase);
+            builder.withKeyStorePassphrase(runContext.render(keyStorePassphrase));
         }
 
         return builder.build();
--- src/main/java/org/kestra/task/kubernetes/models/Metadata.java
+++ src/main/java/org/kestra/task/kubernetes/models/Metadata.java
@@ -5,38 +5,104 @@ import io.fabric8.kubernetes.api.model.ObjectMeta;
 import io.fabric8.kubernetes.api.model.OwnerReference;
 import lombok.Builder;
 import lombok.Getter;
+import org.kestra.core.models.annotations.OutputProperty;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 
 @Builder
 @Getter
 public class Metadata {
-    private Map<String, String> annotations;
-    private String clusterName;
-    private String creationTimestamp;
-    private Long deletionGracePeriodSeconds;
-    private String deletionTimestamp;
-    private List<String> finalizers;
-    private String generateName;
-    private Long generation;
-    private Map<String, String> labels;
-    private List<ManagedFieldsEntry> managedFields;
-    private String name;
-    private String namespace;
-    private List<OwnerReference> ownerReferences;
-    private String resourceVersion;
-    private String selfLink;
+    @OutputProperty(
+        description = "Generated Uid of this resource"
+    )
     private String uid;
+
+    @OutputProperty(
+        description = "Name of the resource"
+    )
+    private final String name;
+
+    @OutputProperty(
+        description = "Namespace of the resource"
+    )
+    private final String namespace;
+
+    @OutputProperty(
+        description = "Name of the current cluster"
+    )
+    private final String clusterName;
+
+    @OutputProperty(
+        description = "List of all annotations of the resource"
+    )
+    private final Map<String, String> annotations;
+
+    @OutputProperty(
+        description = "List of labels"
+    )
+    private final Map<String, String> labels;
+
+    @OutputProperty(
+        description = "Creation datetime"
+    )
+    private final Instant creationTimestamp;
+
+    @OutputProperty(
+        description = "Deletetion grace period in seconds"
+    )
+    private final Long deletionGracePeriodSeconds;
+
+    @OutputProperty(
+        description = "Deletetion datetime"
+    )
+    private final Instant deletionTimestamp;
+
+    @OutputProperty(
+        description = "List of finalizers"
+    )
+    private final List<String> finalizers;
+
+    @OutputProperty(
+        description = "Generate name of the resource"
+    )
+    private final String generateName;
+
+    @OutputProperty(
+        description = "Generation"
+    )
+    private final Long generation;
+
+    @OutputProperty(
+        description = "List of managed fields"
+    )
+    private final List<ManagedFieldsEntry> managedFields;
+
+    @OutputProperty(
+        description = "List of owner reference"
+    )
+    private final List<OwnerReference> ownerReferences;
+
+    @OutputProperty(
+        description = "Resource version"
+    )
+    private final String resourceVersion;
+
+    @OutputProperty(
+        description = "Direct link on the api of this resource"
+    )
+    private final String selfLink;
+
 
     public static Metadata fromObjectMeta(ObjectMeta objectMeta) {
         MetadataBuilder builder = Metadata.builder();
 
         builder.annotations(objectMeta.getAnnotations());
         builder.clusterName(objectMeta.getClusterName());
-        builder.creationTimestamp(objectMeta.getCreationTimestamp());
+        builder.creationTimestamp(Instant.parse(objectMeta.getCreationTimestamp()));
         builder.deletionGracePeriodSeconds(objectMeta.getDeletionGracePeriodSeconds());
-        builder.deletionTimestamp(objectMeta.getDeletionTimestamp());
+        builder.deletionTimestamp(objectMeta.getDeletionTimestamp() != null ? Instant.parse(objectMeta.getDeletionTimestamp()) : null);
         builder.finalizers(objectMeta.getFinalizers());
         builder.generateName(objectMeta.getGenerateName());
         builder.generation(objectMeta.getGeneration());
