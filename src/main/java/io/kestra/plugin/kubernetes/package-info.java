440ec7554ac1ea8a1a920b754595d1c4bd8b4ae9
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:33:26 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/kubernetes/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using Kubernetes.",
+    categories = PluginSubGroup.PluginCategory.MISC //FIXME TOOL
+)
+package io.kestra.plugin.kubernetes;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
