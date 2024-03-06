46ba1b63fc607ebbabb49173e21905dc5072523a
Loïc Mathieu <loikeseke@gmail.com>
2024-03-06 10:02:38 +0100

    feat: add plugin group documentation

---
+++ src/main/java/io/kestra/plugin/kubernetes/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This subgroup of plugins contains tasks for interacting with a Kubernetes cluster.",
+    categories = { PluginSubGroup.PluginCategory.TOOL, PluginSubGroup.PluginCategory.CLOUD }
+)
+package io.kestra.plugin.kubernetes;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
