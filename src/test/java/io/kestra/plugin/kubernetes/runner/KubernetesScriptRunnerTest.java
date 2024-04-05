d36b883bed10aaf8bfcc8c3f43eb430e43cfa22d
brian.mulier <bmmulier@hotmail.fr>
2024-04-05 14:12:35 +0200

    feat(runner): working outputDir

--- src/test/java/io/kestra/plugin/kubernetes/runner/KubernetesScriptRunnerTest.java
+++ src/test/java/io/kestra/plugin/kubernetes/runner/KubernetesScriptRunnerTest.java
@@ -2,17 +2,8 @@ package io.kestra.plugin.kubernetes.runner;
 
 import io.kestra.core.models.script.AbstractScriptRunnerTest;
 import io.kestra.core.models.script.ScriptRunner;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 
 class KubernetesScriptRunnerTest extends AbstractScriptRunnerTest {
-
-    @Test
-    @Disabled("Test issue")
-    @Override
-    protected void inputAndOutputFiles() {
-    }
-
     @Override
     protected ScriptRunner scriptRunner() {
         return KubernetesScriptRunner.builder().build();
