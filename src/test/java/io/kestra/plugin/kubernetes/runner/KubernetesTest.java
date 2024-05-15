3141e178f69938b8d09c705f96b06e1d26f254de
Loïc Mathieu <loikeseke@gmail.com>
2024-05-15 15:53:25 +0200

    feat: rename KubernetesTaskRunner to Kubernetes

--- src/test/java/io/kestra/plugin/kubernetes/runner/KubernetesTest.java
+++ src/test/java/io/kestra/plugin/kubernetes/runner/KubernetesTest.java
@@ -3,9 +3,9 @@ package io.kestra.plugin.kubernetes.runner;
 import io.kestra.core.models.tasks.runners.AbstractTaskRunnerTest;
 import io.kestra.core.models.tasks.runners.TaskRunner;
 
-class KubernetesTaskRunnerTest extends AbstractTaskRunnerTest {
+class KubernetesTest extends AbstractTaskRunnerTest {
     @Override
     protected TaskRunner taskRunner() {
-        return KubernetesTaskRunner.builder().build();
+        return Kubernetes.builder().build();
     }
 }
