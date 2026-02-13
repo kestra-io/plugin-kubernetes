fe9d4a1e594e4e344befa6ea97afb3a8227f75c0
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:04:00 +0100

    chore: update plugin categories (#258)
    * chore: update plugin categories
    
    * chore: update kestraVersion to 1.2.3
    
    ---------
    
    Co-authored-by: ben8t <pimpaudben@gmail.com>
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>
    Co-authored-by: Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>

--- src/main/java/io/kestra/plugin/kubernetes/kubectl/package-info.java
+++ src/main/java/io/kestra/plugin/kubernetes/kubectl/package-info.java
@@ -1,6 +1,6 @@
 @PluginSubGroup(
     description = "This subgroup of plugins contains core tasks for interacting with a Kubernetes cluster through kubectl commands.",
-    categories = { PluginSubGroup.PluginCategory.TOOL, PluginSubGroup.PluginCategory.CLOUD }
+        categories = { PluginSubGroup.PluginCategory.INFRASTRUCTURE }
 )
 package io.kestra.plugin.kubernetes.kubectl;
 
