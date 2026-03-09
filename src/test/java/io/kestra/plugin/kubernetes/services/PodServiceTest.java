06c515609eeaf718e50e9daf68b733681d8dec20
Malay Dewangan <malaydewangan310@gmail.com>
2026-03-09 23:42:56 +0530

    Ensure waitForPodReady only treats Pods as ready when ContainersReady is True

--- src/test/java/io/kestra/plugin/kubernetes/services/PodServiceTest.java
+++ src/test/java/io/kestra/plugin/kubernetes/services/PodServiceTest.java
@@ -57,44 +57,4 @@ class PodServiceTest {
 
         assertNotNull(result);
     }
-
-    @Test
-    void shouldNotReturnPodWhenContainersReadyFalse() {
-        KubernetesClient client = mock(KubernetesClient.class);
-
-        Pod pod = new PodBuilder()
-            .withNewMetadata()
-            .withName("test-pod")
-            .withNamespace("default")
-            .endMetadata()
-            .withNewStatus()
-            .addNewCondition()
-            .withType("ContainersReady")
-            .withStatus("False")
-            .endCondition()
-            .endStatus()
-            .build();
-
-        @SuppressWarnings("rawtypes")
-        MixedOperation pods = mock(MixedOperation.class);
-
-        @SuppressWarnings("rawtypes")
-        NonNamespaceOperation ns = mock(NonNamespaceOperation.class);
-
-        PodResource podResource = mock(PodResource.class);
-
-        when(client.pods()).thenReturn(pods);
-        when(pods.inNamespace("default")).thenReturn(ns);
-        when(ns.withName("test-pod")).thenReturn(podResource);
-
-        when(podResource.waitUntilCondition(any(), anyLong(), any()))
-            .thenAnswer(invocation -> {
-                Predicate<Pod> predicate = invocation.getArgument(0);
-                return predicate.test(pod) ? pod : null;
-            });
-
-        Pod result = PodService.waitForPodReady(client, pod, Duration.ofSeconds(1));
-
-        assertNull(result);
-    }
 }
